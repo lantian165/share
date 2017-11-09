0eb4f6dad3bad93caac7a755fd29255bad6b564f
lantian165 <lantian165@163.com>
2017-11-10 00:35:52 +0800

    add process-tongxin.py

---
+++ python/Demo/8_3-process-tongxin.py
@@ -0,0 +1,37 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from multiprocessing import Process, Queue
+import os, time, random
+
+def write(q):
+    for value in ['A', 'B', 'C']:
+        print 'Put %s to queue...' %value
+        q.put(value)
+        time.sleep(random.random())
+
+def read(q):
+    while True:
+        value = q.get(q)
+        print 'Read %s from queue...' %value
+
+if __name__ == '__main__':
+    q = Queue()
+
+    #创建写进程
+    pw = Process(target=write, args=(q,))
+
+    #创建读进程
+    pr = Process(target=read, args = (q,))
+
+    #启动写进程
+    pw.start()
+
+    #启动读进程
+    pr.start()
+
+    # 等待写进程结束
+    pw.join()
+
+    # 读进程是死循环， 需要强制结束
+    pr.terminate()
