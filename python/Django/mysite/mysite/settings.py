a157edae4e50fb7821f8de09fff81f7b5207ed34
lantian165 <lantian165@163.com>
2017-12-27 01:06:36 +0800

    create a template and render 404 page

--- python/Django/mysite/mysite/settings.py
+++ python/Django/mysite/mysite/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysites',
-        'USER': 'django',
+        'USER': 'root',
         'PASSWORD': '123456',
-        'HOST': '10.100.1.94',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS':{
             'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
--- python/Django/mysite/polls/views.py
+++ python/Django/mysite/polls/views.py
@@ -1,17 +1,64 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.http import Http404
 from django.shortcuts import render
 
 from django.http import HttpResponse
+#from django.template import loader
+
+from .models import Question
 
 # Create your views here.
 
+# Version 1:
+'''
 def index(request):
     return HttpResponse("Hello, baochen. Your's at the polls index.")
+'''
 
+# Version 2:
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
+'''
+
+# Version 3:
+'''
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
+'''
+
+# Version 4: shortcut render()
+# https://docs.djangoproject.com/en/1.11/intro/tutorial03/
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request,'polls/index.html', context)
+
+# Version 1:
+'''
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
+'''
+
+# Version 2:
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+
+    return render(request,'polls/detail.html',{'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
@@ -23,3 +70,7 @@ def vote(request, question_id):
 # This is the simplest view possible in Django.
 # To call the view, we need to map it to a URL
 # - and for this we need a URLconf
+
+# Reading to:
+# url: https://docs.djangoproject.com/en/1.11/intro/tutorial03/
+# A shortcut: get_object_or_404()
